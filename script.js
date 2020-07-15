c5023813b31942f5fd63bd0e1de6cbdbf8cf0d26
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-07-15 06:58:14 +0000

    practice (7/15)

--- script.js
+++ script.js
@@ -96,3 +96,22 @@ switch(color){
 	console.log("要注意");
 	break;
 }
+// default
+const rank = 5;
+switch(rank){
+	case 1:
+	console.log("金メダルです");
+	break;
+
+	case 2:
+	console.log("銀メダルです");
+	break;
+
+	case 3:
+	console.log("銅メダルです");
+	break;
+
+	default:
+	console.log("メダルはありません");
+	break;
+}
