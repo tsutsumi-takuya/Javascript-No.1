1eb5a9bd2ace650fc1990032f2489951944c692a
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-07-25 05:51:14 +0000

    practice (7/25)

--- script(pulldown).js
+++ script(pulldown).js
@@ -1,18 +1,23 @@
 
-var lang = document.querySelector('html').lang;
+var lang = document.querySelector('html').lang; // 'document.querySelector('CSSセレクタ')'
 
 var opt;
-if(lang === 'ja'){
+
+if(lang === 'ja'){ // 日本語ページの場合、変数langには'ja'が入っている為、<option value="index(japanese).html">日本語</option>
 	opt = document.querySelector('option[value="index(japanese).html"]');
 }
-else if(lang === 'en'){
+else if(lang === 'en'){ // 英語ページの場合、変数langには'en'が入っている為、<option value="index(english).html">English</option>
 	opt = document.querySelector('option[value="index(english).html"]');
 }
-else if(lang === 'zh'){
+else if(lang === 'zh'){ // 中文ページの場合、変数langには'zh'が入っている為、<option value="index(chinese).html">中文</option>
 	opt = document.querySelector('option[value="index(chinese).html"]');
 }
 
-opt.selected = true;
+switch(lang){
+
+}
+
+opt.selected = true; // selected属性,check属性はその属性に'true'を代入
 
 document.getElementById('form').select.onchange = function(){ // 'onchange'はフォームに入力された内容が変更された時に発生
 	location.href = document.getElementById('form').select.value; // 'location.href'はURLを書き換えそのページに遷移する
