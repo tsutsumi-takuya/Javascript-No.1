efd2cadc1b3c3f251b4515dc14e72c5855f87f50
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-07-25 05:54:40 +0000

    practice (7/25)

--- script(pulldown).js
+++ script(pulldown).js
@@ -1,23 +1,35 @@
 
 var lang = document.querySelector('html').lang; // 'document.querySelector('CSSセレクタ')'
 
-var opt;
+// var opt;
 
-if(lang === 'ja'){ // 日本語ページの場合、変数langには'ja'が入っている為、<option value="index(japanese).html">日本語</option>
-	opt = document.querySelector('option[value="index(japanese).html"]');
-}
-else if(lang === 'en'){ // 英語ページの場合、変数langには'en'が入っている為、<option value="index(english).html">English</option>
-	opt = document.querySelector('option[value="index(english).html"]');
-}
-else if(lang === 'zh'){ // 中文ページの場合、変数langには'zh'が入っている為、<option value="index(chinese).html">中文</option>
-	opt = document.querySelector('option[value="index(chinese).html"]');
-}
+// if(lang === 'ja'){ // 日本語ページの場合、変数langには'ja'が入っている為、<option value="index(japanese).html">日本語</option>
+// 	opt = document.querySelector('option[value="index(japanese).html"]');
+// }
+// else if(lang === 'en'){ // 英語ページの場合、変数langには'en'が入っている為、<option value="index(english).html">English</option>
+// 	opt = document.querySelector('option[value="index(english).html"]');
+// }
+// else if(lang === 'zh'){ // 中文ページの場合、変数langには'zh'が入っている為、<option value="index(chinese).html">中文</option>
+// 	opt = document.querySelector('option[value="index(chinese).html"]');
+// }
 
 switch(lang){
 
+	case'ja':
+	opt = document.querySelector('option[value="index(japanese).html"]');
+	break;
+
+	case'en':
+	opt = document.querySelector('option[value="index(english).html"]');
+	break;
+
+	case'zh':
+	opt = document.querySelector('option[value="index(chinese).html"]');
+	break;
+
 }
 
-opt.selected = true; // selected属性,check属性はその属性に'true'を代入
+// opt.selected = true; // selected属性,check属性はその属性に'true'を代入
 
 document.getElementById('form').select.onchange = function(){ // 'onchange'はフォームに入力された内容が変更された時に発生
 	location.href = document.getElementById('form').select.value; // 'location.href'はURLを書き換えそのページに遷移する
