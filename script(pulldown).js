9936e6effa1b11ec5a5e8b80c156ddc79d46ebe2
tsutsumi takuya <t1m11.1127.fam@gmail.com>
2020-07-25 05:42:07 +0000

    practice (7/25)

--- script(pulldown).js
+++ script(pulldown).js
@@ -1,3 +1,19 @@
-document.getElementById('form').select.onchange = function(){
-	location.href = document.getElementById('form').select.value;
+
+var lang = document.querySelector('html').lang;
+
+var opt;
+if(lang === 'ja'){
+	opt = document.querySelector('option[value="index(japanese).html"]');
+}
+else if(lang === 'en'){
+	opt = document.querySelector('option[value="index(english).html"]');
+}
+else if(lang === 'zh'){
+	opt = document.querySelector('option[value="index(chinese).html"]');
+}
+
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function(){ // 'onchange'はフォームに入力された内容が変更された時に発生
+	location.href = document.getElementById('form').select.value; // 'location.href'はURLを書き換えそのページに遷移する
 }
